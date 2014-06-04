d126baf9afb5a6200f5d430dd1e7b95b3f1b65d8
StanislavLyalin <lyalinstas@gmail.com>
2014-06-05 00:18:33 +0400

    add simple menu in GoalsFragment

--- app/src/main/java/com/intellilife/app/GoalsFragment.java
+++ app/src/main/java/com/intellilife/app/GoalsFragment.java
@@ -3,6 +3,8 @@ package com.intellilife.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -21,10 +23,23 @@ public class GoalsFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         TextView textView = (TextView) rootView.findViewById(R.id.tabTitleTextView);
         textView.setText(getResources().getString(R.string.goals_tab_title));
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.goals_fragment_menu, menu);
     }
 }
